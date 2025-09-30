344ee34bedf9486791b32b42e211cd20e8bb8c15
Abhishek Kumar Pal <akp1552005@gmail.com>
2025-09-30 13:13:38 +0530

    finally

--- server/index.js
+++ server/index.js
@@ -18,14 +18,7 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
-// Handle preflight requests
-app.options('/*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
-});
+
 
 // Add request logging
 app.use((req, res, next) => {
