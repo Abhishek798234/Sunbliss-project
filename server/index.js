f49fc2d92c0f3d917e25fe9ad9ffec6e6e3632b9
Abhishek Kumar Pal <akp1552005@gmail.com>
2025-09-30 12:54:50 +0530

    index.js fixed

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-app.options('*', (req, res) => {
+app.options('/*', (req, res) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
